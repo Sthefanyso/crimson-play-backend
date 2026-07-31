b81e55a3779d551d654868fa68f8a0747cfb3090
Sthefanyso <sthefanydsilva29@gmail.com>
2026-07-30 23:19:49 -0300

    refactor: start of the migration from JavaScript to TypeScript

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import moviesRoutes from "./modules/movies/movies.routes";
+import seriesRoutes from "./modules/series/series.routes";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rota de teste
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "API do TMDB conectada ao Crimson Play, funcionou!",
+  });
+});
+
+app.use("/api/movies", moviesRoutes);
+app.use("/api/series", seriesRoutes);
+
+export default app;
